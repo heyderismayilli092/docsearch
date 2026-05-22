f34c142c8a966067bb529310cdd1519e4948a67c
Heydar Ismayilli <heyderismayilli092@gmail.com>
2026-05-23 01:17:04 +0400

    A list box was added and integrated with the system file lister function

--- src/main.py
+++ src/main.py
@@ -9,6 +9,7 @@ import os
 import locale
 import docsearch
 import docextract
+from docsearch_functions import files_list
 
 locale.bindtextdomain('pardus-belgeara', '/usr/share/locale')
 locale.textdomain('pardus-belgeara')
@@ -24,11 +25,30 @@ class pardusbelgeara:
         # -------Widget referansları-------
         # Main Window
         self.mainwindow = self.builder.get_object("mainwindow")  # home window
+        self.listbox    = self.builder.get_object("listbox")  # listbox object
 
         # -------Signals-------
         # Main Window
         self.mainwindow.connect("destroy", self._on_destroy)
         self.mainwindow.show_all()
+
+        for f in files_list():
+            row = self.create_row(os.path.basename(f), f)
+            self.listbox.add(row)
+        self.listbox.show_all()
+
+
+    # row function to be created for each data point
+    def create_row(self, filename, fullpath):
+        row_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=10)
+
+        label = Gtk.Label(label=filename)
+        button_open = Gtk.Button(label="Open")
+
+        row_box.pack_start(label, True, True, 5)
+        row_box.pack_end(button_open, False, False, 5)
+        return row_box
+
 
     def _on_destroy(self, widget):
         Gtk.main_quit()
