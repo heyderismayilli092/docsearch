4f078846b5ba286aed595f46dd555eced26a0ed4
Heydar Ismayilli <heyderismayilli092@gmail.com>
2026-05-22 21:12:40 +0400

    The main code file has been created

---
+++ src/main.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk, GObject
+
+import os
+import locale
+import docsearch
+import docextract
+
+locale.bindtextdomain('pardus-belgeara', '/usr/share/locale')
+locale.textdomain('pardus-belgeara')
+
+GLADE_FILE = os.path.dirname(os.path.abspath(__file__)) + "/../ui/MainWindow.glade"
+
+
+class pardus_belgeara:
+    def __init__(self):
+        self.builder = Gtk.Builder()
+        self.builder.add_from_file(GLADE_FILE)
+
+        # -------Widget referansları-------
+        # Main Window
+        self.mainwindow = self.builder.get_object("mainwindow")  # home window
+
+        # -------Signals-------
+        # Main Window
+        self.mainwindow.connect("destroy", self._on_destroy)
+
+
+    def _on_destroy(self, widget):
+        Gtk.main_quit()
+
+app = pardus_belgeara()
+Gtk.main()
+
